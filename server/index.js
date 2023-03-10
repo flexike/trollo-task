419768c329c8d7727cff5da94d6d1ed2e939a2fc
flexike <flexike@ukr.net>
2023-03-10 23:30:51 +0200

    +backend-checks

--- server/index.js
+++ server/index.js
@@ -10,61 +10,81 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get("/", async (req, res) => {
-  const displayAll = await prisma.taskTable.findMany({
-    include: {
-      tasks: true,
-    },
-  });
-  res.status(200).json(displayAll);
-  console.log(displayAll);
+  try {
+    const displayAll = await prisma.taskTable.findMany({
+      include: {
+        tasks: true,
+      },
+    });
+    res.status(200).json(displayAll);
+    console.log(displayAll);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.post("/create/table", async (req, res) => {
-  const { title } = req.body;
-  const newTable = await prisma.taskTable.create({
-    data: {
-      title: title,
-    },
-  });
-  res.status(200).redirect("/");
-  console.log(newTable);
+  try {
+    const { title } = req.body;
+    const newTable = await prisma.taskTable.create({
+      data: {
+        title: title,
+      },
+    });
+    res.status(200).redirect("/");
+    console.log(newTable);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.post("/create/task", async (req, res) => {
-  const { title, description, author, tableId } = req.body;
-  const newTask = await prisma.tasks.create({
-    data: {
-      title: title,
-      description: description,
-      author: author,
-      taskTableId: parseInt(tableId),
-    },
-  });
-  console.log(req.body.taskTableId);
-  res.status(200).json(newTask);
+  try {
+    const { title, description, author, tableId } = req.body;
+    const newTask = await prisma.tasks.create({
+      data: {
+        title: title,
+        description: description,
+        author: author,
+        taskTableId: parseInt(tableId),
+      },
+    });
+    console.log(req.body.taskTableId);
+    res.status(200).json(newTask);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.delete("/delete/table", async (req, res) => {
-  const { id } = req.body;
-  const deletedTable = await prisma.taskTable.delete({
-    where: {
-      id: id,
-    },
-    include: {
-      tasks: true,
-    },
-  });
-  res.status(200).json(deletedTable);
+  try {
+    const { id } = req.body;
+    const deletedTable = await prisma.taskTable.delete({
+      where: {
+        id: id,
+      },
+      include: {
+        tasks: true,
+      },
+    });
+    res.status(200).json(deletedTable);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.delete("/delete/task", async (req, res) => {
-  const { id } = req.body;
-  const deletedTask = await prisma.tasks.delete({
-    where: {
-      id: id,
-    },
-  });
-  res.status(200).json(deletedTask);
+  try {
+    const { id } = req.body;
+    const deletedTask = await prisma.tasks.delete({
+      where: {
+        id: id,
+      },
+    });
+    res.status(200).json(deletedTask);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.listen(3001, () => {
